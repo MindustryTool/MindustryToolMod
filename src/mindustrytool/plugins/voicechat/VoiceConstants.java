8b47b084a4d55a4bfe741968bcee9ade51800e50
sharrlotte <nguyennhonhau25052003@gmail.com>
2025-12-27 12:55:37 +0900

    feat: add voice chat constants and update mod version

--- src/mindustrytool/plugins/voicechat/VoiceConstants.java
+++ src/mindustrytool/plugins/voicechat/VoiceConstants.java
@@ -7,8 +7,8 @@ public final class VoiceConstants {
 
     public static final int SAMPLE_RATE = 48000;
     public static final int CHANNELS = 1;
-    public static final int BUFFER_SIZE = 1920; // 40ms at 48kHz (Safe for encoder, reduced PPS)
-    public static final int CAPTURE_INTERVAL_MS = 40;
+    public static final int BUFFER_SIZE = 2880; // 60ms at 48kHz (Reduces PPS from 25 to 17)
+    public static final int CAPTURE_INTERVAL_MS = 60;
 
     private VoiceConstants() {
     }
