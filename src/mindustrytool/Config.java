93cdf1d3451beb97c36e183587b0f6b4e91ba960
Sharlotte <nguyennhonhau25052003@gmail.com>
2026-03-30 00:45:25 +0700

    chore: update Discord invite URL to website redirect link
    The old Discord invite link has been replaced with a more maintainable
    redirect URL that points to the same destination. This allows for
    easier link management in the future without requiring code changes.

--- src/mindustrytool/Config.java
+++ src/mindustrytool/Config.java
@@ -27,7 +27,7 @@ public class Config {
     public static final String UPLOAD_SCHEMATIC_URL = WEB_URL + "/schematics?upload=true";
     public static final String UPLOAD_MAP_URL = WEB_URL + "/maps?upload=true";
 
-    public static final String DISCORD_INVITE_URL = "https://discord.gg/DmjdPCeWPY";
+    public static final String DISCORD_INVITE_URL = "https://mindustry-tool.com/links/mindustry-tool";
 
     public static final List<Sort> sorts = Arrays.asList(//
             new Sort("newest", "time_desc"), //
