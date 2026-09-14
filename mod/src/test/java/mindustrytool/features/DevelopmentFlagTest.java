4915d620334251869db761a7b7cfefb5e911e2a2
Sharlotte <nguyennhonhau25052003@gmail.com>
2026-09-14 20:11:03 +0700

    test(DevelopmentFlagTest): remove unused imports and obsolete test

--- mod/src/test/java/mindustrytool/features/DevelopmentFlagTest.java
+++ mod/src/test/java/mindustrytool/features/DevelopmentFlagTest.java
@@ -9,20 +9,6 @@ import arc.mock.MockSettings;
 import arc.scene.style.TextureRegionDrawable;
 import mindustry.gen.Icon;
 import mindustrytool.features.autoplay.AutoplayFeature;
-import mindustrytool.features.godmode.GodModeFeature;
-import mindustrytool.features.healthbar.HealthBarFeature;
-import mindustrytool.features.music.MusicFeature;
-import mindustrytool.features.pathfinding.PathfindingFeature;
-import mindustrytool.features.playerconnect.PlayerConnectFeature;
-import mindustrytool.features.prettychat.PrettyChatFeature;
-import mindustrytool.features.progressdisplay.ProgressDisplayFeature;
-import mindustrytool.features.rangedisplay.RangeDisplayFeature;
-import mindustrytool.features.savesync.SaveSyncFeature;
-import mindustrytool.features.smartdrill.SmartDrillFeature;
-import mindustrytool.features.smartupgrade.SmartUpgradeFeature;
-import mindustrytool.features.timecontrol.TimeControlFeature;
-import mindustrytool.features.togglerendering.ToggleRenderingFeature;
-import mindustrytool.features.wavepreview.WavePreviewFeature;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -86,54 +72,6 @@ class DevelopmentFlagTest {
                 .build();
 
         assertFalse(metadata.isDevelopment());
-    }
-
-    @Test
-    void allStubsAreMarkedDevelopment() {
-        Feature[] stubs = {
-                new PlayerConnectFeature(),
-                new HealthBarFeature(),
-                new PathfindingFeature(),
-                new RangeDisplayFeature(),
-                new PrettyChatFeature(),
-                new AutoplayFeature(),
-                new WavePreviewFeature(),
-                new SaveSyncFeature(),
-                new GodModeFeature(),
-                new SmartDrillFeature(),
-                new SmartUpgradeFeature(),
-                new MusicFeature(),
-                new ProgressDisplayFeature(),
-                new ToggleRenderingFeature(),
-                new TimeControlFeature()
-        };
-
-        String[] ids = {
-                "player-connect",
-                "health-bar",
-                "pathfinding",
-                "range-display",
-                "pretty-chat",
-                "autoplay",
-                "wave-preview",
-                "save-sync",
-                "item-visualizer",
-                "god-mode",
-                "smart-drill",
-                "smart-upgrade",
-                "music",
-                "progress-display",
-                "toggle-rendering",
-                "time-control"
-        };
-
-        assertEquals(ids.length, stubs.length);
-
-        for (int i = 0; i < stubs.length; i++) {
-            assertTrue(stubs[i].getMetadata().isDevelopment(), ids[i] + " must be in development");
-            assertEquals(ids[i], stubs[i].getMetadata().getId());
-            assertNotNull(stubs[i].getMetadata().getIcon(), ids[i] + " must declare an icon");
-        }
     }
 
     @Test
