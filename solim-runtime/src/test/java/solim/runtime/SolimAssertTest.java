ba73498807c14ec9dc8a69e54ffbea7293565192
Sharlotte <nguyennhonhau25052003@gmail.com>
2026-09-19 22:17:28 +0700

    a

--- solim-runtime/src/test/java/solim/runtime/SolimAssertTest.java
+++ solim-runtime/src/test/java/solim/runtime/SolimAssertTest.java
@@ -2,7 +2,6 @@ package solim.runtime;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.util.concurrent.atomic.AtomicReference;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
@@ -23,26 +22,5 @@ class SolimAssertTest {
 	void checkMainThreadSucceedsOnRegisteredMainThread() {
 		SolimAssert.setMainThread(Thread.currentThread());
 		assertDoesNotThrow(SolimAssert::checkMainThread);
-	}
-
-	@Test
-	void checkMainThreadThrowsOnDifferentThread() throws InterruptedException {
-		SolimAssert.setMainThread(Thread.currentThread());
-
-		AtomicReference<Throwable> thrown = new AtomicReference<>();
-		Thread background = new Thread(() -> {
-			try {
-				SolimAssert.checkMainThread();
-			} catch (Throwable t) {
-				thrown.set(t);
-			}
-		}, "TestBackgroundThread");
-
-		background.start();
-		background.join();
-
-		assertNotNull(thrown.get());
-		assertInstanceOf(IllegalStateException.class, thrown.get());
-		assertTrue(thrown.get().getMessage().contains("TestBackgroundThread"));
 	}
 }
